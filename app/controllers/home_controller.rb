16259ec8640e2f1f59c3af305c06607408c9ab17
feipinghuang <huang900107@163.com>
2012-11-23 18:45:38 +0800

    fix id s

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @groups = current_user.groups
     @following = current_user.followed_users
     @followers = current_user.followers
+
+    @ids = current_user.group_ids << current_user.id
   end
 end
